a86e3fdc05c7ee207171dd56844370557ebac71a
Tony McLaughlin <tonymclaughlin72@gmail.com>
2019-05-13 19:31:08 -0400

    Added additional scaffolding

--- clicknifeClient/src/app/app.module.ts
+++ clicknifeClient/src/app/app.module.ts
@@ -3,12 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainHeaderComponent } from './shared/components/main-header/main-header.component';
+import { HeroComponent } from './shared/components/hero/hero.component';
+import { PortfolioComponent } from './shared/components/portfolio/portfolio.component';
+import { ContactComponent } from './shared/components/contact/contact.component';
+import { WorkComponent } from './modules/home/pages/work/work.component';
+import { CareersComponent } from './modules/home/pages/careers/careers.component';
+import { DomainComponent } from './modules/home/pages/domain/domain.component';
+import { CompanyComponent } from './modules/home/pages/company/company.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { MainHeaderComponent } from './core/main-header/main-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainHeaderComponent
+    HeroComponent,
+    PortfolioComponent,
+    ContactComponent,
+    WorkComponent,
+    CareersComponent,
+    DomainComponent,
+    CompanyComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainHeaderComponent,
   ],
   imports: [
     BrowserModule,
